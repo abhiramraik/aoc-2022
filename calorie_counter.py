23a3b5f59c2a861bdf2302a8c34d08d135c694f3
Abhiram <a.raikotedadi@cartelsol.com>
2022-12-07 16:05:46 +0100

    AOC challenge day 1 solution

---
+++ calorie_counter.py
@@ -0,0 +1,39 @@
+def max_calories():
+    with open('calorie.txt','r') as input_file:
+        data_str = input_file.read()
+        data_array = data_str.split('\n\n') # Split on all instances of double new lines
+
+        sum_calories = []
+        for values in data_array:
+            values = values.split('\n')
+            total_calories = 0
+            for calories in values:
+                total_calories = total_calories + int(calories)
+            sum_calories.append(total_calories)
+        return max(sum_calories)
+
+def calories_three_elves():
+    with open('calorie.txt','r') as input_file:
+        data_str = input_file.read()
+        data_array = data_str.split('\n\n') # Split on all instances of double new lines
+
+        sum_calories = []
+        for values in data_array:
+            values = values.split('\n')
+            total_calories = 0
+            for calories in values:
+                total_calories = total_calories + int(calories)
+            sum_calories.append(total_calories)
+
+        sum_calories.sort(reverse=True)
+        return(sum_calories[0]+sum_calories[1]+sum_calories[2])
+
+print('Maximum calorie by an elf:', 
+      max_calories())
+
+print('Maximum calorie by three elves:',
+      calories_three_elves())
+
+
+
+        

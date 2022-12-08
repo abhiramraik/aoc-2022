a881802ea7164b8b4bd875f7bffeaec033f1dd6e
Abhiram <a.raikotedadi@cartelsol.com>
2022-12-08 12:19:34 +0100

    chaalenge 4 part two solution

--- 2022_Challenge_4/assignment_pair.py
+++ 2022_Challenge_4/assignment_pair.py
@@ -1,10 +1,10 @@
 import csv
 
-def count_assignement_pair():
+def expanded_list_func():
     pair_list = []
     full_list = []
     expanded_list = []
-    total_count = 0
+    
 
     for pairs in csv.reader(open('2022_Challenge_4/assignment_pair.txt')):
         pair_list.append(pairs[0].split('-'))
@@ -21,7 +21,12 @@ def count_assignement_pair():
         else:
             temp_list = [full_list[values]]
             expanded_list.append(temp_list)
+    return expanded_list
 
+    
+def count_assignement_pair():
+    expanded_list = expanded_list_func()
+    total_count = 0
     for sets in range(0, len(expanded_list),2):
         if all(item in expanded_list[sets] for item in expanded_list[sets+1]):
             total_count +=1
@@ -29,7 +34,15 @@ def count_assignement_pair():
             total_count +=1
     return total_count
 
+def count_overlap_pairs():
+    expanded_list = expanded_list_func()
+    total_count = 0
+
+    for sets in range(0, len(expanded_list),2):
+        if len(set(expanded_list[sets]).intersection(expanded_list[sets+1]))>=1:
+            total_count +=1
+    return total_count
+
 
 print('Duplicate Assignemet pair is =', count_assignement_pair())
-
-
+print('Overlap Assignemet pair is =', count_overlap_pairs())
